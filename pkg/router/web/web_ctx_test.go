dc6da74226d625a427c9423b534c8d815cffa8e2
idea-your-father <1957476540@qq.com>
2021-12-26 15:31:45 +0800

    添加测试方法

--- pkg/router/web/web_ctx_test.go
+++ pkg/router/web/web_ctx_test.go
@@ -2,36 +2,48 @@ package web
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"testing"
+	"webmvc/pkg/router"
 )
 
 func Test_run_server(t *testing.T) {
-	router := DefaultRouter()
+	r := DefaultRouter()
 	//自定义 context工厂，自己可以修改 里面的实现方法
-	router.CtxFactory = func(w http.ResponseWriter, r *http.Request, p router.Params) Ctx {
+	r.CtxFactory = func(w http.ResponseWriter, r *http.Request, p router.Params) Ctx {
 		return &CtxDefault{
 			w, r, p,
 		}
 	}
-	router.Router.ServeFiles("./static", http.Dir("./"))
+	//后置处理器，用于处理返回的结果
+	r.PostResultHandler = []PostHandler{
+		func(ctx Ctx, res interface{}) bool {
+			if res != nil {
+				log.Println("用来打印日志......")
+				return true
+			}
+			return true
+		},
+	}
+	r.Router.ServeFiles("./static", http.Dir("./"))
 	//404 找不到页面
-	router.Router.OnNotFound(func(w http.ResponseWriter, R *http.Request) {
+	r.Router.OnNotFound(func(w http.ResponseWriter, R *http.Request) {
 		fmt.Fprintf(w, "找不到页面了 !!!!")
 	})
-	router.Router.OnMethodNotAllowed(func(w http.ResponseWriter, R *http.Request) {
+	r.Router.OnMethodNotAllowed(func(w http.ResponseWriter, R *http.Request) {
 
 		fmt.Fprintf(w, "请求方法不正确 !!!!")
 	})
-	router.GET("/echo", nil, func(c Ctx) interface{} {
+	r.GET("/echo", nil, func(c Ctx) interface{} {
 		fmt.Fprintf(c.Response(), "这是个 返回！！！")
 		return nil
 	})
-	router.POST("/test", nil, func(c Ctx) interface{} {
+	r.POST("/test", nil, func(c Ctx) interface{} {
 
 		fmt.Fprintf(c.Response(), "这是个 返回！！！")
 		return nil
 	})
 
-	router.Listen(8080)
+	r.Listen(8080)
 }
